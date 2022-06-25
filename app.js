ae48da9a7ada85bd0f59786409ee2b4fe1d69616
Olawale <olawaleadesoye4@gmail.com>
2022-06-25 14:34:23 +0200

    Bio

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let name = "ADESOYE ABDULAZEEZ OLAWALE "
+let height = "6.0 FEET"
+let country = "NIGERIA"
+alert("MY NAME  IS " + name + "   ");
+alert("MY HEIGHT IS " + height + "  ");
+alert("I AM FROM " + country);
---
+++ style.js
@@ -0,0 +1,1 @@
+document.writeln("I am 20 yrs")
